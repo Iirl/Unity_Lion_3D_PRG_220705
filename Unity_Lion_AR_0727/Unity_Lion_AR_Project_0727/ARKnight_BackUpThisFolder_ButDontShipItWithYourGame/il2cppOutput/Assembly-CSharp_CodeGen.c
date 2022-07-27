b04e022f5b4d73fae0fe5b65b46011e891587332
Iirl <102848236+Iirl@users.noreply.github.com>
2022-07-27 16:27:09 +0800

    220727: 製作完成

---
+++ Unity_Lion_AR_0727/Unity_Lion_AR_Project_0727/ARKnight_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,65 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void AR.Target.CardManage::Awake()
+extern void CardManage_Awake_mC3202E793AF90EBEC8876B13B6722FBD64B85977 (void);
+// 0x00000002 System.Void AR.Target.CardManage::PressJump(Vuforia.VirtualButtonBehaviour)
+extern void CardManage_PressJump_m3ACA03C97D1E31F7DC62281DE1B483CB54BFFC9F (void);
+// 0x00000003 System.Void AR.Target.CardManage::CardFond()
+extern void CardManage_CardFond_mEE5F30ECFF153952AB222E7E64CBCD09C9596F85 (void);
+// 0x00000004 System.Void AR.Target.CardManage::CardLost()
+extern void CardManage_CardLost_mB5F8461549D9184EB3AB4FDB89F4A65D16E9BC6D (void);
+// 0x00000005 System.Void AR.Target.CardManage::Attack()
+extern void CardManage_Attack_m48E86AB1CC44F2EF87F5B5176BD54BBE8D319EEE (void);
+// 0x00000006 System.Void AR.Target.CardManage::.ctor()
+extern void CardManage__ctor_mB7C79CEACF4F2559D8061502105117DA42063FA5 (void);
+static Il2CppMethodPointer s_methodPointers[6] = 
+{
+	CardManage_Awake_mC3202E793AF90EBEC8876B13B6722FBD64B85977,
+	CardManage_PressJump_m3ACA03C97D1E31F7DC62281DE1B483CB54BFFC9F,
+	CardManage_CardFond_mEE5F30ECFF153952AB222E7E64CBCD09C9596F85,
+	CardManage_CardLost_mB5F8461549D9184EB3AB4FDB89F4A65D16E9BC6D,
+	CardManage_Attack_m48E86AB1CC44F2EF87F5B5176BD54BBE8D319EEE,
+	CardManage__ctor_mB7C79CEACF4F2559D8061502105117DA42063FA5,
+};
+static const int32_t s_InvokerIndices[6] = 
+{
+	4823,
+	3833,
+	4823,
+	4823,
+	4823,
+	4823,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	6,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
